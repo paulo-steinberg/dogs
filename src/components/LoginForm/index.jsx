c13224f8d3a04bade060c2269d7c9bf10c059d1f
Paulo Steinberg <paulo@physiotec.org>
2021-08-03 15:03:35 -0400

    Creating basic auth.

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,3 +1,38 @@
+import { LoginCreate } from "../LoginCreate";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { api } from "../../services/api";
+
 export const LoginForm = () => {
-  return <h1>Login Form</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    api
+      .post("jwt-auth/v1/token", { username, password })
+      .then((res) => console.log(res.data.token));
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="text"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Send</button>
+      </form>
+
+      <Link to="/login/create">
+        <LoginCreate />
+      </Link>
+    </section>
+  );
 };
